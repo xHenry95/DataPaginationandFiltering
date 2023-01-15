0c047a985c70f0151b7a8714001515dd7f42cba5
HenryCS <henry.cs@hotmail.co.uk>
2023-01-15 15:53:00 +0000

    Completed the core function of pag buttons working
    Additional commends provided

--- js/script.js
+++ js/script.js
@@ -4,18 +4,12 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 // GOING FOR EXCEEDS EXPECTATIONS :) 
 
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+/**
+ * Creating the showPage() function: 
+ *  first setting the number of students per page
+ *  then using this number to calculate the range of indices to display
+ * @param {arr} list list of data to pass into the function
+ * @param {number} page page selection used to calculate the necessary indices
 */
 //set page length of 9
 const pageLength = 9;
@@ -48,15 +42,38 @@ function showPage(list, page) {
       }
    }
 }
-
-
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 //calculate how many pages are needed by dividing the data.length by page length 
 // (with 42 items and 9 items per page, this should be around 4.6 pages - although this will be dynamic)
+function addPagination(list) {
+   const numOfBtns = list.length / pageLength;
+   const linkListUl = document.querySelector('.link-list');
+   linkListUl.innerHTML = '';
+   for ( let i = 0; i < numOfBtns; i++ ) {
+      const li = document.createElement('li');
+      const button = document.createElement('button');
+      button.className = 'button';
+      button.textContent = i+1;
+      li.appendChild(button);
+      linkListUl.appendChild(li);
+   }
+   const pagBtn1 = document.querySelector('.button');
+   pagBtn1.classList.add('active');
+   linkListUl.addEventListener('click', (e) => {
+      if ( e.target.tagName === 'BUTTON' ) {
+         const currentActivtBtn = document.getElementsByClassName('active')[0];
+         currentActivtBtn.classList.remove('active');
+         e.target.classList.add('active');
+         showPage(list, e.target.textContent);
+      }
+   })
+}
+
 
 
 // Call functions
 showPage(data, 1);
+addPagination(data);
